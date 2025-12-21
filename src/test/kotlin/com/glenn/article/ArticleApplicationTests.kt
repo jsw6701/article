379f23010d93e839f5200cf750ed3438bd94f91f
Glenn <anes53027@gmail.com>
2025-12-21 18:27:20 +0900

    Initial commit

---
+++ src/test/kotlin/com/glenn/article/ArticleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.glenn.article
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArticleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
